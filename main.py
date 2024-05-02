378c2b5380f536e2010d130bbc01436abb6f117d
JGMontanez <joseMontanez@unomaha.edu>
2024-05-02 14:43:49 -0500

    added 3 files
    added main
    added logic
    added gui

---
+++ main.py
@@ -0,0 +1,13 @@
+# remake of lab 01
+from logic import *
+
+
+#TODO: create gui window that displlays vote menu
+#TODO: create logic file
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
