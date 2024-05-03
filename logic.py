7744bbfe1874c46e9f90e3b61ccf5257f87bca35
JGMontanez <joseMontanez@unomaha.edu>
2024-05-03 09:58:09 -0500

    Update logic.py
    added submit function
    development in progress

--- logic.py
+++ logic.py
@@ -7,4 +7,31 @@ class Logic(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        self.button_submit.clicked.connect(lambda:self.submit())
+
+    def submit(self):
+        """
+        Function to write vote to csv file
+        :return:
+        """
+        try:
+            #validate id
+            id = int(self.input_id.text())
+            #if ID is not 5 numbers,
+            if len(str(id)) !=5:
+                raise ValueError
+            #elif id in file #if the id is in csv file raise an error
+
+
+
+
+        except ValueError:
+            self.label_msg.setText(f'Invalid ID. Please enter 5 digit ID.')
+
+        except:
+            self.label_msg.setText(f'Invalid ID.')
+
+
+
+
 
